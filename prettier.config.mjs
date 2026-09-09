fdbbcfe6c600923312fb88515e22603e566ce074
Nikita Sadovikov <denregt9@gmail.com>
2026-09-09 18:39:16 +0500

    chore: добавлен ESLint и Prettier конфиги, обновлены зависимости

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+const prettierConfig = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 2,
+  endOfLine: 'lf',
+};
+
+export default prettierConfig;
